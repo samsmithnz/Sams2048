626391cfd9dd2af3a06ccdd7ebe6b275caae00f3
Sam Smith <samsmithnz@gmail.com>
2021-11-28 16:10:18 -0500

    Added game won/lost conditions

---
+++ 2048.Logic/Game.cs
@@ -0,0 +1,30 @@
+﻿namespace _2048.Logic
+{
+    public class Game
+    {
+        public Game(int x = 5, int y = 5)
+        {
+            GameBoard = new int[x, y];
+        }
+
+        public int[,] GameBoard { get; set; }
+
+        public bool CheckIfGameIsComplete()
+        {
+            int xLength = GameBoard.GetLength(0);
+            int yLength = GameBoard.GetLength(1);
+
+            for (int x = 0; x < xLength; x++)
+            {
+                for (int y = 0; y < yLength; y++)
+                {
+                    if (GameBoard[x, y] == 2048)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
