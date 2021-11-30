9b52e586b1b62296f0db0549d598c6b7cf446e2a
Sam Smith <samsmithnz@gmail.com>
2021-11-29 21:21:18 -0500

    Added additional tests

--- 2048.Tests/GameAddPieceTests.cs
+++ 2048.Tests/GameAddPieceTests.cs
@@ -41,20 +41,130 @@ namespace _2048.Tests
 ";
             Assert.AreEqual(gameBoardExpected, gameBoardCurrent);
         }
+        [TestMethod]
+        public void AddPieceWithNoSpacesLeftTest()
+        {
+            //Arrange
+            string initialBoard = @"
+---------
+|1|2|4|8|
+---------
+|1|2|4|8|
+---------
+|1|2|4|8|
+---------
+|1|2|4|8|
+---------
+";
+            Game game = new(initialBoard);
 
-        //[TestMethod]
-        //public void GameIsNotWonYetTest()
-        //{
-        //    //Arrange
-        //    Game game = new();
-        //    game.GameBoard[0, 0] = 1;
+            //Act
+            game.AddNewPiece();
+            string gameBoardCurrent = game.ToString();
 
-        //    //Act
-        //    bool gameIsComplete = game.CheckIfGameIsComplete();
+            //Assert
+            string gameBoardExpected = @"
+---------
+|1|2|4|8|
+---------
+|1|2|4|8|
+---------
+|1|2|4|8|
+---------
+|1|2|4|8|
+---------
+";
+            Assert.AreEqual(gameBoardExpected, gameBoardCurrent);
+        }
 
-        //    //Assert
-        //    Assert.IsFalse(gameIsComplete);
-        //}
+        [TestMethod]
+        public void Add16PiecesTest()
+        {
+            //Arrange
+            string initialBoard = @"
+---------
+|0|0|0|0|
+---------
+|0|0|0|0|
+---------
+|0|0|0|0|
+---------
+|0|0|0|0|
+---------
+";
+            Game game = new(initialBoard);
 
+            //Act
+            game.AddNewPiece();
+            string gameBoardCurrent1 = game.ToString();
+
+            //Assert
+            string gameBoardExpected1 = @"
+---------
+|0|0|0|0|
+---------
+|0|0|0|0|
+---------
+|0|0|1|0|
+---------
+|0|0|0|0|
+---------
+";
+            Assert.AreEqual(gameBoardExpected1, gameBoardCurrent1);
+
+            //Act 2
+            game.AddNewPiece();
+            string gameBoardCurrent2 = game.ToString();
+
+            //Assert 2
+            string gameBoardExpected2 = @"
+---------
+|0|0|0|0|
+---------
+|0|0|0|0|
+---------
+|0|0|1|1|
+---------
+|0|0|0|0|
+---------
+";
+            Assert.AreEqual(gameBoardExpected2, gameBoardCurrent2);
+
+            //Act 3
+            game.AddNewPiece();
+            string gameBoardCurrent3 = game.ToString();
+
+            //Assert 3
+            string gameBoardExpected3 = @"
+---------
+|0|0|0|0|
+---------
+|0|0|0|0|
+---------
+|0|1|1|1|
+---------
+|0|0|0|0|
+---------
+";
+            Assert.AreEqual(gameBoardExpected3, gameBoardCurrent3);
+
+            //Act 4
+            game.AddNewPiece();
+            string gameBoardCurrent4 = game.ToString();
+
+            //Assert 4
+            string gameBoardExpected4 = @"
+---------
+|0|0|0|0|
+---------
+|0|0|0|0|
+---------
+|1|1|1|1|
+---------
+|0|0|0|0|
+---------
+";
+            Assert.AreEqual(gameBoardExpected4, gameBoardCurrent4);
+        }
     }
 }
